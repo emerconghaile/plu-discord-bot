acf085c02f92491602d47b22ce8b24f98574920a
Emer Conghaile <emerconn7@gmail.com>
2021-04-08 08:39:36 -0500

    !quiz, !help, !about, new csv regex
    !quiz has been added, with a PLU option\n!help and !about now use template literals for easer format reading\nAll instances of 'Retailer Assigned' have been removed using shell regex

--- csvREGEX.sh
+++ csvREGEX.sh
@@ -1,3 +1,3 @@
  #!/usr/bin/env bash
 
-awk -v FS=\",\" '{ for (i=1;i<=5;i++) printf "\"" $i "\","; printf "\n" }' Commodities_20210406090430.csv | sed 's/^.//;s/.$//' > allPLUcodes.csv
+awk '! /Retailer Assigned/' Commodities_20210406090430.csv | awk -v FS=\",\" '{ for (i=1;i<=5;i++) printf "\"" $i "\","; printf "\n" }' | sed 's/^.//;s/.$//' > allPLUcodes.csv
--- my_bot.js
+++ my_bot.js
@@ -1,19 +1,32 @@
 // Load the secret bot token
-const token = require('./token.json');
+const token = require('./token.json')
 // Load the fast-csv parse module
 const fs = require('fs')
 const csv = require('@fast-csv/parse')
 // Parse csv file into an array
-const dataArrFile = "allPLUcodes.csv"
-const dataArr = [];
-csv.parseFile(dataArrFile, {headers: true})
-.on("data", data => {
-  dataArr.push(data);
-})
-.on("end", () => {
-  console.log(`Finished parsing '${dataArrFile}' for a total of ${dataArr.length} lines\n`);
-  //console.log(dataArr)
-});
+const csvFileName = "allPLUcodes.csv"
+const allPLUcodes = []
+csv.parseFile(csvFileName, {headers: true})
+    .on("data", data => {
+    allPLUcodes.push(data)
+    })
+    .on("end", () => {
+    console.log(`Finished parsing '${csvFileName}' for a total of ${allPLUcodes.length} lines\n`)
+    });
+
+// create new arrays by Category
+// Dried Fruits array
+/*varietyParse = () => {
+    allDriedFruitsCodes = []
+    console.log(allPLUcodes.length)
+    for (i=0;i<=allPLUcodes.length;i++) {
+        console.log(allPLUcodes[i])
+        if (allPLUcodes[i].VARIETY.toUpperCase() === "Dried Fruits") {
+            allDriedFruitsCodes.push(allPLUcodes[i])
+        }
+    }
+    console.log(allDriedFruitsCodes)
+}*/
 
 // Load the discord bot modules
 const Discord = require('discord.js')
@@ -25,9 +38,9 @@ client.on('ready', () => {
     // User, channel, & server totals
     console.log(`Users: ${client.users.cache.size} | Channels: ${client.channels.cache.size} | Servers: ${client.guilds.cache.size}`)
     // List all servers the bot is connected to
-    console.log("Server Names:")
+    console.log("Servers:")
     client.guilds.cache.forEach((guild) => {
-        console.log(` - ${guild.name} [${guild.id}]`)
+        console.log(` - [${guild.id}] ${guild.name}`)
 
 
         client.user.setActivity("made by Emer Conghaile");
@@ -58,49 +71,107 @@ processCommand = message => {
     let primaryCommand = splitCommand[0]
     let arguments = splitCommand.slice(1)
 
-    console.log("\nServer: " + message.guild.name)
-    console.log("Channel: " + message.channel.name)
-    console.log("User: " + message.author)
-    console.log("Command: " + primaryCommand)
-    console.log("Arguments: " + arguments)
+    console.log(`\n${"Server:".padEnd(11)}[${message.guild.id}] ${message.guild.name}`)
+    console.log(`${"Channel:".padEnd(11)}[${message.channel.id}] ${message.channel.name}`)
+    console.log(`${"User:".padEnd(11)}[${message.author.id}] ${message.author.tag}`)
+    console.log(`${"Command:".padEnd(11)}${primaryCommand}`)
+    console.log(`${"Arguments:".padEnd(11)}${arguments}`)
 
     if (primaryCommand == "help") {
         helpCommand(arguments, message)
+    } else if (primaryCommand == "about") {
+        aboutCommand(arguments, message)
     } else if (primaryCommand == "search") {
         searchCommand(arguments, message)
-    } else if (primaryCommand == "github") {
-        githubCommand(arguments, message)
+    } else if (primaryCommand == "quiz") {
+        quizCommand(arguments, message)
     } else {
-        message.channel.send("Invalid command, type !help")
+        message.channel.send("Invalid command, type '!help'")
     }
 }
 // !help
 helpCommand = (arguments, message) => {
-    message.channel.send("Current valid commands: 1) !search")
+    message.author.send(
+`Valid Commands:
+
+- !help
+- !search plu [number]
+- !search variety [name]
+- !quiz plu`
+    )
+}
+// !about
+aboutCommand = (arguments, message) => {
+    message.author.send(
+`Type !help to see all commands
+    
+Made by Emer Conghaile
+https://github.com/emerconghaile/plu-discord-bot/`
+    )
 }
 // !search
 searchCommand = (arguments, message) => {
-    if (arguments[0] == "PLU" && arguments.length >= 2) {
-        let obj = dataArr.find(obj => obj.PLU == arguments[1])
-        console.log(obj)
-        if ( typeof obj !== "undefined") {
-            if (obj.VARIETY) {
-                message.channel.send(`${obj.COMMODITY}, ${obj.VARIETY}, ${obj.SIZE}`)
-            } else {
-                message.channel.send(`${obj.COMMODITY}, -- , ${obj.SIZE}`)
+    if (arguments.length > 0) {
+        if (arguments[0].toUpperCase() === "PLU") {
+            if (arguments.length >= 2) {
+                let obj = allPLUcodes.find(obj => obj.PLU == arguments[1])
+                console.log(obj)
+                if ( typeof obj !== "undefined") {
+                    if (obj.VARIETY) {
+                        message.channel.send(`${obj.COMMODITY}, ${obj.VARIETY}, ${obj.SIZE}`)
+                    } else {
+                        message.channel.send(`${obj.COMMODITY}, -- , ${obj.SIZE}`)
+                    }
+                } else {
+                    message.channel.send("Invalid PLU")
+                }
+            } else if (arguments.length <= 1) {
+                message.channel.send("Missing PLU")
+            }
+        } else if (arguments[0].toUpperCase() === "VARIETY") {
+            if (arguments.length >= 2) {
+                let obj = allPLUcodes.find(obj => obj.VARIETY.toUpperCase() == arguments[1].toUpperCase())
+                console.log(obj)
+                if ( typeof obj !== "undefined") {
+                    if (obj.VARIETY) {
+                        message.channel.send(`${obj.COMMODITY}, ${obj.VARIETY}, ${obj.SIZE}`)
+                    } else {
+                        message.channel.send(`${obj.COMMODITY}, -- , ${obj.SIZE}`)
+                    }
+                } else {
+                    message.channel.send("Invalid variety")
+                }
+            } else if (arguments.length <= 1) {
+                message.channel.send("Missing variety")
             }
         } else {
-            message.channel.send("Invalid PLU")
+            message.channel.send("Invalid command, type '!help'")
         }
-    } else if (arguments[0] == "PLU" && arguments.length <= 1) {
-        message.channel.send("Missing PLU number")
     } else {
-        message.channel.send("Invalid command, try '!search PLU (number)'")
+        message.channel.send("Invalid command, type '!help'")
     }
 }
-// !github
-githubCommand = (arguments, message) => {
-    message.author.send("https://github.com/emerconghaile/plu-discord-bot/")
+// !quiz
+quizCommand = (arguments, message) => {
+    if (arguments.length > 0) {
+        if (arguments[0].toUpperCase() === "PLU") {
+            let randomIndex = Math.floor(Math.random() * allPLUcodes.length)
+            console.log(randomIndex)
+            let randomObject = allPLUcodes[randomIndex]
+            console.log(randomObject)
+            message.channel.send(`What is PLU ${randomObject.PLU}?`)
+        } else if (arguments[0].toUpperCase() === "VARIETY") {
+            if (arguments.length >= 2) {
+
+            } else if (arguments.length <= 1) {
+                message.channel.send("Missing variety")
+            }
+        } else {
+            message.channel.send("Invalid command, type '!help'")
+        }
+    } else {
+        message.channel.send("Invalid command, type '!help'")
+    }
 }
 
 // Bot login
